8cf936ad2a70f6c26e6203941534d713cbd3a523
mhShohan <mehdihasanshohan25@gmail.com>
2026-02-21 10:58:26 +0600

    nexxt client setup and login and register page

---
+++ next-client/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from 'next';
+import { IBM_Plex_Mono, Inter, Libre_Baskerville } from 'next/font/google';
+import './globals.css';
+
+const fontSans = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+const fontSerif = Libre_Baskerville({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ['400', '700'],
+});
+
+const fontMono = IBM_Plex_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+});
+
+export const metadata: Metadata = {
+  title: 'Inventory Management System',
+  description: 'A simple inventory management system built with Next.js and Tailwind CSS.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${fontSans.variable} ${fontMono.variable} ${fontSerif.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
