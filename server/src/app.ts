2647a28d5ad98bc60c1b895c00a71b8d59420ef0
Sampath252005 <sampathnaik65@gmail.com>
2026-04-29 08:32:39 +0530

    ...

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use(cors({ origin: ['http://localhost:5173', 'https://inventory-navy.vercel.app', 'http://localhost:3000'] }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://mongo-db-project-6n9u.vercel.app', 'http://localhost:3000'] }));
 
 // application routes
 app.use('/api/v1', rootRouter);
